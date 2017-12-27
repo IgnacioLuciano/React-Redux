8e71b874ea67296334f854bcddf34e68b5deff27
Unknown <iglumu@gmail.com>
2017-12-27 15:51:15 -0800

    added 1 botton

--- src/index.js
+++ src/index.js
@@ -2,28 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import Counter from './counter.js';
+import { connect, Provider } from "react-redux";
+
 
 const initialState = {
-    count = 0
+    count : 0
 }
+const mapStateToProps = (state) => { return { count: state.count } }
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onIncrement: () => dispatch({ type: 'INCREMENT' }),
+        // onIncrement2 : () => dispatch({ type: 'INCREMENT2' }),
+        onReset : () => dispatch({ type: 'RESET' }),
+        onDecrement : () => store.dispatch({ type: "DECREMENT" }),
+        // onIncrement2 : () => store.dispatch({ type: 'DECREMENT2' }),
+    }
+}
+
+const ConnectedCounter = connect(mapStateToProps, mapDispatchToProps)(Counter);
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-
         case 'RESET':
             return { ...state, count: 0 };
 
         case 'INCREMENT':
             return { ...state, count: state.count + 5 };
 
-        case 'INCREMENT2':
-            return { ...state, count: state.count + 10 };
+        // case 'INCREMENT2':
+            // return { ...state, count: state.count + 10 };
 
         case 'DECREMENT':
             return { ...state, count: state.count - 2 };
 
-        case 'DECREMENT2':
-            return { ...state, count: state.count - 7 };
+        // case 'DECREMENT2':
+            // return { ...state, count: state.count - 7 };
 
         default:
             return state;
@@ -32,26 +45,10 @@ const reducer = (state = initialState, action) => {
 
 const store = createStore(reducer);
 
-const RenderApp = () => {
-
     ReactDOM.render(
-        <Counter
-            count={store.getState().count1}
-            onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-            onIncrement2={() => store.dispatch({ type: 'INCREMENT2' })}
-            onReset={() => store.dispatch({ type: 'RESET' })}
-            onDecrement={() => store.dispatch({ type: "DECREMENT" })}
-            onIncrement2={() => store.dispatch({ type: 'DECREMENT2' })}
+        <Provider store={store}>
+           <ConnectedCounter />
+        </Provider>,
 
-        />,
         document.getElementById('root')
     )
-};
-
-store.subscribe(() => {
-    console.log("State Value: ", store.getState());
-    renderApp();
-}
-);
-
-renderApp();
